b62364df7400c42d211a8f51af0316bb2990cfe9
Dhruval Patel <68235648+DhruvalVP@users.noreply.github.com>
2024-08-05 15:47:09 +0530

    Create 2_3_numbers.py

---
+++ 1. Beginner/Numbers/2_3_numbers.py
@@ -0,0 +1,8 @@
+# If you cross a 490 meterlong street in 7 minutes, calculate your speed in meters per second. Print the answer without any decimal point in it. Hint: Speed = Distance / Time
+
+street_len = 490    # meter
+time_taken = 7  # minutes
+
+speed = int(street_len/time_taken)  # calculate speed to integer value
+
+print(f"Speed is {speed} meters per second.")
