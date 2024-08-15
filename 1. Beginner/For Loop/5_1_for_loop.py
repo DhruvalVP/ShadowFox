6b1aa6cc9e833506e7927de021b31c0ec20ab69c
Dhruval Patel <68235648+DhruvalVP@users.noreply.github.com>
2024-08-15 10:58:27 +0530

    Update 5_1_for_loop.py

--- 1. Beginner/For Loop/5_1_for_loop.py
+++ 1. Beginner/For Loop/5_1_for_loop.py
@@ -3,7 +3,7 @@
 import random
 
 # ----- Set the number of rolls ----- #
-num_rolls = 5
+num_rolls = 20
 
 # ----- Initialize counters for specific roll values and two consecutive 6s ----- #
 num_rolls_6 = 0
@@ -15,23 +15,26 @@ prev_roll = 0
 for _ in range(num_rolls):
     # ----- Simulate a dice roll ----- #
     
-    # dice_roll = random.randint(1, 6)
-    dice_roll = int(input("Enter dice number: "))
-    prev_roll = dice_roll
+    dice_roll = random.randint(1, 6)
+    # dice_roll = int(input("Enter dice number: "))
+    print(dice_roll, end=" ")
 
     # ----- Increment the counter if the roll is a 6 or 1 ----- #
     if dice_roll == 6:
         num_rolls_6 += 1
     
+    elif dice_roll == 1:
+        num_rolls_1 += 1
+
     # ----- Check if two 6s have been rolled consecutively ----- #
     if prev_roll == 6 and dice_roll == 6:
         consecutive_6s += 1
     
-    elif dice_roll == 1:
-        num_rolls_1 += 1
+
+    prev_roll = dice_roll
 
 # ----- Print the number of times each event occurred during the specified number of rolls ----- #
-print(f"Number of times rolled a 6: {num_rolls_6}")
+print(f"\nNumber of times rolled a 6: {num_rolls_6}")
 print(f"Number of times rolled a 1: {num_rolls_1}")
 print(f"Number of times rolled two 6s in a row: {consecutive_6s}")
 
